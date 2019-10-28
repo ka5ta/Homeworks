b7f524fd90694107eae62d5c0bfbcf95a9e43d21
Kasia <k.urbanska87@gmail.com>
2019-10-28 21:40:16 +0100

    +days finished

--- src/DayOfTheWeek.java
+++ src/DayOfTheWeek.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+
 public class DayOfTheWeek {
+
+    private static String[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+    private static void dayAfterNumberDays(int number, String day) {
+        int weekdayNumberFromUser = Arrays.asList(weekDays).indexOf(day);
+        //        int weekdayAfterNumberDays = (number + weekdayNumberFromUser)% 7;
+        int weekdayAfterNumberDays = number + weekdayNumberFromUser;
+
+        //        System.out.println("Your day is: "+day + " after "+number+" days is: " +weekDays[weekdayAfterNumberDays]);
+
+        if(weekdayAfterNumberDays>=7){
+                int newWeekdayNumber = weekdayAfterNumberDays % 7;
+                String newDayName = weekDays[newWeekdayNumber];
+                System.out.println("Your day is: "+day + " after "+number+" days is: " +newDayName);
+            }else{
+                System.out.println("Your day is: "+day + " after "+number+" days is: " +weekDays[number+weekdayNumberFromUser]);
+            }
+    }
+
+    public static void main(String[] args) {
+        dayAfterNumberDays(2,"Wednesday");
+    }
 }
